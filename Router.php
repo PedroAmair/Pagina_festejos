cddec2ebb9895b400ea973f47fb2e6f6ed94b6af
Pedro Amair <pedroamair@hotmail.com>
2022-11-26 19:38:21 -0400

    Modificacion al router.php

--- Router.php
+++ Router.php
@@ -19,8 +19,12 @@ class Router
 
     public function comprobarRutas()
     {
-        $currentUrl = $_SERVER["REQUEST_URI"] === "" ? "/" : $_SERVER["REQUEST_URI"];
-        $method = $_SERVER["REQUEST_METHOD"];
+        if (isset($_SERVER['PATH_INFO'])) {
+            $currentUrl = $_SERVER['PATH_INFO'] ?? '/';
+        } else {
+            $currentUrl = strtok($_SERVER['REQUEST_URI'], '?') ?? '/';
+        }
+        $method = $_SERVER['REQUEST_METHOD'];
 
         if ($method === "GET") {
             $fn = $this->getRoutes[$currentUrl] ?? null;
