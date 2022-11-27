3704fe5e6fd43b15078979d6cc8479c18ba98d29
Pedro Amair <pedroamair@hotmail.com>
2022-11-27 09:59:04 -0400

    Cambio en Router.php

--- Router.php
+++ Router.php
@@ -19,11 +19,7 @@ class Router
 
     public function comprobarRutas()
     {
-        if (isset($_SERVER['PATH_INFO'])) {
-            $currentUrl = $_SERVER['PATH_INFO'] ?? '/';
-        } else {
-            $currentUrl = strtok($_SERVER['REQUEST_URI'], '?') ?? '/';
-        }
+        $currentUrl = strtok($_SERVER['REQUEST_URI'], '?') ?? '/';
         $method = $_SERVER['REQUEST_METHOD'];
 
         if ($method === "GET") {
