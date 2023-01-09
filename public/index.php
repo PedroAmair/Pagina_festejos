d019a9d5b04858300a3b6b32810e7eadf0d46510
Pedro Amair <pedroamair@hotmail.com>
2023-01-09 10:01:51 -0400

    cambios en el require de index

--- public/index.php
+++ public/index.php
@@ -1,6 +1,5 @@
 <?php
-    var_dump(__DIR__);
-    require_once __DIR__."/../includes/app.php";
+    require_once __DIR__."/../../includes/app.php";
 
     use MVC\Router;
     use Controllers\LoginController;
