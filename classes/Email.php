6e34864c2343e609f95e714475172aed4f675a12
Pedro Amair <pedroamair@hotmail.com>
2025-04-21 13:55:18 -0400

    cambios en los endpoints para las API's

--- classes/Email.php
+++ classes/Email.php
@@ -38,7 +38,7 @@ class Email {
 
          $contenido = '<html>';
         $contenido .= "<p><strong>Hola " . $this->nombre .  "</strong> Has Creado tu cuenta en Club de festejos Merina, con el usuario ". $this->email. " solo debes confirmarla presionando el siguiente enlace</p>";
-         $contenido .= "<p>Presiona aquí: <a href='https://festejosmerina-f3b9f9cf6e47.herokuapp.com/confirmar-cuenta?token=" . $this->token . "'>Confirmar Cuenta</a>";        
+         $contenido .= "<p>Presiona aquí: <a href='https://web-festejos.up.railway.app/confirmar-cuenta?token=" . $this->token . "'>Confirmar Cuenta</a>";        
          $contenido .= "<p>Si tu no has solicitaste este cambio, puedes ignorar el mensaje</p>";
          $contenido .= '</html>';
          $mail->Body = $contenido;
@@ -69,7 +69,7 @@ class Email {
 
         $contenido = '<html>';
         $contenido .= "<p><strong>Hola " . $this->nombre .  "</strong> Has solicitado reestablecer tu password, sigue el siguiente enlace para hacerlo.</p>";
-        $contenido .= "<p>Presiona aquí: <a href='https://festejosmerina-f3b9f9cf6e47.herokuapp.com/recuperar?token=" . $this->token . "'>Reestablecer Password</a>";        
+        $contenido .= "<p>Presiona aquí: <a href='https://web-festejos.up.railway.app/recuperar?token=" . $this->token . "'>Reestablecer Password</a>";        
         $contenido .= "<p>Si tu no solicitaste este cambioo, puedes ignorar este mensaje</p>";
         $contenido .= '</html>';
         $mail->Body = $contenido;
